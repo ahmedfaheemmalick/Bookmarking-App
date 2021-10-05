b8c1a177f70819b606ecf1db93f07a0e4808e336
AhmedMalick <ahmedmalik2k20@gmail.com>
2021-10-05 14:35:05 +0500

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "/.netlify/functions/bookmark",
+    uri: "/.netlify/functions",
     fetch,
   }),
   cache: new InMemoryCache(),
